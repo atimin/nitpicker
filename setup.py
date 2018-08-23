144e6898da0faf39f2f541b57a5184233cce0f2f
Aleksey Timin <atimin@gmail.com>
2018-08-22 20:22:02 -0700

    Fix setup.py script.

--- setup.py
+++ setup.py
@@ -6,19 +6,16 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='nitpicker',
-    version='0.0.1',
-    description='CLI tool for QA',
+    version='0.0.2',
+    description='A simple CLI tool for QA',
     long_description=readme,
     author='Aleksey Timin',
     author_email='atimin@gmail.com',
     url='https://github.com/flipback/nitpicker',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
+    license='MIT',
+    packages=find_packages(exclude=('tests', 'docs', 'qa')),
     python_requires='>=3.3'
 )
 
