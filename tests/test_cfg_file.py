f2ef81a5948f656f6d3f5a614f9668cdaf95bef1
Aleksey Timin <atimin@gmail.com>
2018-10-26 20:50:40 +0500

    Fix tests.

--- tests/test_cfg_file.py
+++ tests/test_cfg_file.py
@@ -14,7 +14,7 @@ class TestCfgFile(unittest.TestCase):
         self.__ctx = Context()
 
     def test_load_settings_from_cfg_file(self):
-        __main_imp__(self.__ctx, qa_dir=None, no_editor=None, report_dir=None, cvs=None, debug=None, main_branch=None,
+        __main_imp__(self.__ctx, qa_dir=None, no_editor=None, report_dir=None, vcs=None, debug=None, main_branch=None,
                      cfg_file=self.__cfg_file)
 
         self.assertEqual(self.__ctx.obj['qa_dir'], 'test_qa')
@@ -25,19 +25,19 @@ class TestCfgFile(unittest.TestCase):
         self.assertEqual(self.__ctx.obj['main_branch'], 'test_branch')
 
     def test_command_option_override_cfg_file(self):
-        __main_imp__(self.__ctx, qa_dir='option_dir', no_editor=False, report_dir='option_report', cvs='option_cvs',
+        __main_imp__(self.__ctx, qa_dir='option_dir', no_editor=False, report_dir='option_report', vcs='option_vcs',
                      debug=False, main_branch='option_branch',
                      cfg_file=self.__cfg_file)
 
         self.assertEqual(self.__ctx.obj['qa_dir'], 'option_dir')
         self.assertEqual(self.__ctx.obj['no_editor'], False)
         self.assertEqual(self.__ctx.obj['report_dir'], 'option_report')
-        self.assertEqual(self.__ctx.obj['vcs'], 'option_cvs')
+        self.assertEqual(self.__ctx.obj['vcs'], 'option_vcs')
         self.assertEqual(self.__ctx.obj['debug'], False)
         self.assertEqual(self.__ctx.obj['main_branch'], 'option_branch')
 
     def test_using_default_settings_if_config_not_exists(self):
-        __main_imp__(self.__ctx, qa_dir=None, no_editor=None, report_dir=None, cvs=None,
+        __main_imp__(self.__ctx, qa_dir=None, no_editor=None, report_dir=None, vcs=None,
                      debug=None, main_branch=None,
                      cfg_file='xxxxxxxxxxx')
 
@@ -49,7 +49,7 @@ class TestCfgFile(unittest.TestCase):
         self.assertEqual(self.__ctx.obj['main_branch'], 'master')
 
     def test_using_default_settings_if_config_has_no_valid_settings(self):
-        __main_imp__(self.__ctx, qa_dir=None, no_editor=None, report_dir=None, cvs=None,
+        __main_imp__(self.__ctx, qa_dir=None, no_editor=None, report_dir=None, vcs=None,
                      debug=None, main_branch=None,
                      cfg_file='.nitpicker.yml.wrong')
 
